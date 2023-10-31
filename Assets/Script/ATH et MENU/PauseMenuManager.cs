bf2485e6c11d96d8f5d6458beb4433b836f385e0
noahhrcy <148714569+noahhrcy@users.noreply.github.com>
2023-10-31 17:56:38 +0100

    Menu échap fonctionnel ( sauf parametres)

---
+++ Assets/Script/ATH et MENU/PauseMenuManager.cs
@@ -0,0 +1,40 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class PauseMenuManager : MonoBehaviour
+{
+    public GameObject pauseMenuUI;
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            if (Time.timeScale == 1f)
+            {
+                PauseGame();
+            }
+            else
+            {
+                ResumeGame();
+            }
+        }
+    }
+
+    public void PauseGame()
+    {
+        pauseMenuUI.SetActive(true);
+        Time.timeScale = 0f;
+    }
+
+    public void ResumeGame()
+    {
+        pauseMenuUI.SetActive(false);
+        Time.timeScale = 1f;
+    }
+
+    public void QuitToMainMenu()
+    {
+        Time.timeScale = 1f; // Assurez-vous de remettre le TimeScale à 1 lorsque vous quittez
+        SceneManager.LoadScene("MenuPrincipal"); // Chargez votre scène de menu principal
+    }
+}
