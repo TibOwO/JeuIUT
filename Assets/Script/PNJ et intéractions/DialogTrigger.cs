f1f28eac1fba29efee37330320340606246b7a00
TibOwO <thibaultamettler@gmail.com>
2023-12-18 20:37:46 +0100

    Résolution de bug (pnj de départ))

--- Assets/Script/PNJ et intéractions/DialogTrigger.cs
+++ Assets/Script/PNJ et intéractions/DialogTrigger.cs
@@ -1,9 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
 using UnityEngine;
 
 public class DialogTrigger : MonoBehaviour
 {
     public Dialog dialog;
     public bool isInRange = false;
+
     void Update()
     {
         if (Input.GetKeyDown(KeyCode.E) && isInRange)
@@ -17,13 +20,22 @@ public class DialogTrigger : MonoBehaviour
         if (collision.CompareTag("Player"))
         {
             isInRange = true;
+            Debug.Log("Le joueur est dans la zone de dialogue");
         }
+    }
 
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        if (collision.CompareTag("Player"))
+        {
+            TriggerExit();
+        }
     }
 
     public void TriggerExit()
     {
         isInRange = false;
+        Debug.Log("Le joueur a quitté la zone de dialogue");
     }
 
     public void TriggerDialog()
