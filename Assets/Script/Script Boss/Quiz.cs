5d1d3aa122edb264fc8a427a2c6b1b27a5094eb1
Lucie Zoppi <lucie.zoppi@etu.univ-amu.fr>
2024-01-15 12:57:03 +0100

    affichage des reponses
    les reponses s'affiche sur les boutons mais quand je clique ca marche pas, jessaie de trouver lerreur

--- Assets/Script/Script Boss/Quiz.cs
+++ Assets/Script/Script Boss/Quiz.cs
@@ -8,17 +8,13 @@ using UnityEngine.UI;
 public class Quiz : MonoBehaviour
 {
     public TextMeshProUGUI txtQuestion;
-    public TextMeshProUGUI txtBtnG;
-    public TextMeshProUGUI txtBtnD;
+    public Button btnReponseG;
+    public Button btnReponseD;
     public string conn;
 
     public string Reponse;
     public TypeWriter typeWriter;
     public List<Question> questions;
-    public GameObject ZoneReponse1;
-    public GameObject ZoneReponse2;
-
-    public ButtonController buttonController;
 
     public struct Question
     {
@@ -37,12 +33,10 @@ public class Quiz : MonoBehaviour
     void Awake()
     {
         txtQuestion = GameObject.Find("txtQuestion").GetComponent<TextMeshProUGUI>();
-        txtBtnG = GameObject.Find("TxtG").GetComponent<TextMeshProUGUI>();
-        txtBtnD = GameObject.Find("TxtD").GetComponent<TextMeshProUGUI>();
+        btnReponseG = GameObject.Find("ReponseG").GetComponent<Button>();
+        btnReponseD = GameObject.Find("ReponseD").GetComponent<Button>();
 
-        buttonController = GetComponent<ButtonController>();
-
-        if (txtQuestion == null || txtBtnG == null || txtBtnD == null || buttonController == null)
+        if (txtQuestion == null || btnReponseG == null || btnReponseD == null)
         {
             Debug.LogError("Un ou plusieurs composants d'interface utilisateur ne sont pas attachés au script.");
         }
@@ -58,57 +52,64 @@ public class Quiz : MonoBehaviour
     }
 
     void Update()
+{
+    // jessaie de debuger
+    if (Input.GetMouseButtonDown(0))
     {
-        if (Input.GetMouseButtonDown(0))
+        Vector3 mousePos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        Collider2D hitCollider = Physics2D.OverlapPoint(mousePos);
+
+       
+        if (btnReponseG != null && btnReponseD != null)
         {
-            Vector3 mousePos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
-            Collider2D hitCollider = Physics2D.OverlapPoint(mousePos);
             if (hitCollider != null)
             {
-                if (hitCollider.gameObject == ZoneReponse1)
+                if (hitCollider.gameObject == btnReponseG.gameObject)
                 {
-                    buttonController.CheckAnswer(txtBtnG.text);
+                    CheckAnswer(btnReponseG.GetComponentInChildren<TextMeshProUGUI>().text);
                 }
-                else if (hitCollider.gameObject == ZoneReponse2)
+                else if (hitCollider.gameObject == btnReponseD.gameObject)
                 {
-                    buttonController.CheckAnswer(txtBtnD.text);
+                    CheckAnswer(btnReponseD.GetComponentInChildren<TextMeshProUGUI>().text);
                 }
             }
         }
+        else
+        {
+            Debug.LogError("Un ou plusieurs boutons ne sont pas initialisés.");
+        }
     }
+}
 
-    public class ButtonController : MonoBehaviour
+ public class ButtonController : MonoBehaviour
     {
-        public Button ReponseG;
-        public Button ReponseD;
-
-        public Quiz quiz;
+        private Quiz quiz; 
 
         void Start()
         {
-            ReponseG.onClick.AddListener(() => CheckAnswer(quiz.txtBtnG.text));
-            ReponseD.onClick.AddListener(() => CheckAnswer(quiz.txtBtnD.text));
+            quiz = FindObjectOfType<Quiz>(); 
+
+            if (quiz == null)
+            {
+                Debug.LogError("La référence à l'instance de Quiz est nulle.");
+                return;
+            }
+
+            
+            GetComponent<Button>().onClick.AddListener(OnClick);
         }
 
-        public void CheckAnswer(string selectedAnswer)
+        void OnClick()
         {
-            if (selectedAnswer == quiz.Reponse)
+     
+            if (quiz != null)
             {
-                Debug.Log("Correct!");
-                quiz.txtQuestion.text = "Gagné!";
+                quiz.CheckAnswer(GetComponentInChildren<TextMeshProUGUI>().text);
             }
-            else
-            {
-                Debug.Log("Incorrect!");
-                quiz.txtQuestion.text = "Perdu!";
-            }
-            quiz.PoseUneQuestion();
         }
     }
 
-
-
-    // Méthode pour charger les questions depuis la base de données
+ 
     void LoadQuestionsFromDatabase()
     {
         questions = new List<Question>();
@@ -130,7 +131,7 @@ public class Quiz : MonoBehaviour
                         List<string> answers = new List<string>();
                         string correctAnswer = "";
 
-                        // Utilisez un nouveau SqliteCommand pour la requête des réponses
+                        
                         using (var answerCmd = dbConnection.CreateCommand())
                         {
                             answerCmd.CommandText = "SELECT * FROM ANSWERS WHERE ParentID = @ParentID";
@@ -142,7 +143,7 @@ public class Quiz : MonoBehaviour
                                 {
                                     string answerText = answerReader.GetString(1);
                                     answers.Add(answerText);
-                                    if (answerReader.GetInt32(3) == 1) // isCorrect
+                                    if (answerReader.GetInt32(3) == 1) 
                                     {
                                         correctAnswer = answerText;
                                     }
@@ -150,7 +151,7 @@ public class Quiz : MonoBehaviour
                             }
                         }
 
-                        // Ajout de la question et des réponses à la liste
+                        
                         questions.Add(new Question(questionText, answers, correctAnswer));
                     }
                 }
@@ -161,20 +162,38 @@ public class Quiz : MonoBehaviour
 
 
     // Méthode pour poser une question
-    void PoseUneQuestion()
+   void PoseUneQuestion()
+{
+    if (questions.Count == 0)
     {
-        if (questions.Count == 0)
+        Debug.LogError("Aucune question n'a été chargée de la base de données.");
+        return;
+    }
+
+    // Sélection aléatoire d'une question
+    var currentQuestion = questions[Random.Range(0, questions.Count)];
+    Reponse = currentQuestion.CorrectAnswer;
+
+    typeWriter.SetText(currentQuestion.Text); // Affiche la question
+
+    
+    btnReponseG.GetComponentInChildren<TextMeshProUGUI>().text = currentQuestion.Answers[0]; // Affiche la première réponse
+    btnReponseD.GetComponentInChildren<TextMeshProUGUI>().text = currentQuestion.Answers[1]; // Affiche la deuxième réponse
+}
+
+
+public void CheckAnswer(string selectedAnswer)
+    {
+        if (selectedAnswer == Reponse)
         {
-            Debug.LogError("Aucune question n'a été chargée de la base de données.");
-            return;
+            Debug.Log("Correct !");
+            txtQuestion.text = "Gagné !";
         }
-
-        // Sélection aléatoire d'une question
-        var currentQuestion = questions[Random.Range(0, questions.Count)];
-        Reponse = currentQuestion.CorrectAnswer;
-
-        typeWriter.SetText(currentQuestion.Text); // Affiche la question
-        txtBtnG.text = currentQuestion.Answers[0]; // Affiche la première réponse
-        txtBtnD.text = currentQuestion.Answers[1]; // Affiche la deuxième réponse
+        else
+        {
+            Debug.Log("Incorrect !");
+            txtQuestion.text = "Perdu !";
+        }
+        PoseUneQuestion();
     }
 }
