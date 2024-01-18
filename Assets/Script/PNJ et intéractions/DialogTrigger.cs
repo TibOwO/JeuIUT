c7d7a1eebf691cc2ecf9cc0c3b3ee259602af582
TibOwO <thibaultamettler@gmail.com>
2024-01-18 06:31:08 +0100

    cleanshit

--- Assets/Script/PNJ et intéractions/DialogTrigger.cs
+++ Assets/Script/PNJ et intéractions/DialogTrigger.cs
@@ -12,17 +12,16 @@ public class DialogTrigger : MonoBehaviour
     public bool isBoss = false;
     public bool isDoor = false;
 
-    // Liste des noms des objets à ramasser (dynamique)
     public List<string> requiredItems = new List<string>();
 
     void Update()
     {
         if (Input.GetKeyDown(KeyCode.E) && isInRange)
         {
-            // Vérifier si c'est un boss et si les objets nécessaires sont dans l'inventaire
+
             if (isBoss && CheckRequiredItems())
             {
-                // Déclencher la scène de combat ici
+
                 StartBossFight();
             }
             else if (isDoor && !CheckRequiredItems())
@@ -63,7 +62,6 @@ public class DialogTrigger : MonoBehaviour
 
     public void TriggerDialog()
     {
-        // Ajout de la condition pour vérifier si l'objet est actif (visible)
         if (gameObject.activeSelf)
         {
             FindObjectOfType<DialogManager>().StartDialog(dialog);
@@ -72,22 +70,21 @@ public class DialogTrigger : MonoBehaviour
 
     private bool CheckRequiredItems()
     {
-        // Utiliser la liste de noms d'objets configurables depuis l'Inspector
         foreach (string itemName in requiredItems)
         {
             bool hasItem = ElementalInventory.Instance.contains(itemName, 1);
             Debug.Log(ElementalInventory.Instance.convertToString());
             if (!hasItem)
             {
-                return false; // Si l'un des objets n'est pas présent, retournez false
+                return false;
             }
         }
 
-        // Si tous les objets sont présents, retournez vrai
+
         return true;
     }
 
-    // Fonction pour déclencher la scène de combat avec le boss
+
     private void StartBossFight()
     {
         SceneManager.LoadScene(gameObject.name);
